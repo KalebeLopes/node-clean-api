6e0bb0ab856a855c1903f635401ecdf5413e25ff
KalebeLopes <calbkalebe@gmail.com>
2021-06-01 10:34:16 -0300

    test: ensure SignUpCOntroller return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -10,7 +10,8 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
-    const httpResonse = sut.handle(httpRequest)
-    expect(httpResonse.statusCode).toBe(400)
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
