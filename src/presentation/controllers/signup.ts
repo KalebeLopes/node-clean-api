d77ddf2a9815e8b1152af81c0146826bb54eb29e
KalebeLopes <calbkalebe@gmail.com>
2021-06-01 10:46:52 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,31 +1,35 @@
-// interface HttpRequest {
-//   body: {
-//     name?: String,
-//     email?: String,
-//     password?: String,
-//     passwordConfirmation?: String
-//   }
-// }
+interface HttpRequest {
+  body: {
+    name?: String,
+    email?: String,
+    password?: String,
+    passwordConfirmation?: String
+  }
+}
 
 export class SignUpController {
 
-  handle(httpRequest: any): any {
-    // const filds = ['name', 'email', 'password', 'passwordConfirmation']
-    
-    // let code = {
-    //   statusCode: 200
+  handle(httpRequest: HttpRequest): any {
+    const filds = ['name', 'email', 'password', 'passwordConfirmation']
+    let obj
+
+    filds.forEach(key => {
+      if(!httpRequest.body[key]) {
+        obj =  {
+          statusCode: 400,
+          body: new Error(`Missing param: ${key}`)
+        }
+
+        return obj
+      }
+    })
+    return obj
+
+    // {
+    //   statusCode: 400,
+    //   body: new Error('Missing param: name')
     // }
-
-    // filds.forEach(key => {
-    //   if(httpRequest.body[key] == null) {
-    //     return code.statusCode = 400
-    //   }
-    // })
-
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
-    }
   }
 
 }
+ 
